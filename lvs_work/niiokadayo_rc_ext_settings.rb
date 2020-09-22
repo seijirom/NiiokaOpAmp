ca03ed7a6788216f44c8a8a757e28e4a637343c4
Seijiro Moriyama <seijiro.moriyama@anagix.com>
2020-09-22 16:09:47 +0900

    compensation capacitor connection changed

--- lvs_work/niiokadayo_rc_ext_settings.rb
+++ lvs_work/niiokadayo_rc_ext_settings.rb
@@ -1,10 +1,13 @@
 def rc_ext_settings
-  same_circuits 'niioka_new', '.TOP'
+  same_circuits 'niioka_new', 'OPAMP_K_NIIOKA_20200903'
   align
   same_device_classes 'PMOS', 'PCH'
   same_device_classes 'NMOS', 'NCH'
   same_device_classes 'HRES', 'RES'
   same_device_classes 'RES', 'RES'
+  tolerance 'HRES', 'R', relative: 0.03
+  tolerance 'RES', 'R', relative: 0.03
+  tolerance 'CAP', 'C', relative: 0.03
   netlist.flatten_circuit 'Nch*'
   netlist.flatten_circuit 'Pch*'
   netlist.flatten_circuit 'R_poly*'
